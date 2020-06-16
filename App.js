a2daba165875201b204bc011419a805d6fe6872a
Antonio Hernández <antoniohernandez95@gmail.com>
2020-06-16 01:18:12 -0400

    Agregado el webview y actualizado el gitignore

--- App.js
+++ App.js
@@ -1,19 +1,11 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { WebView } from "react-native-webview";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <WebView
+      source={{ uri: "https://delivery.pwcc.cl" }}
+      style={{ marginTop: 25 }}
+    />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
